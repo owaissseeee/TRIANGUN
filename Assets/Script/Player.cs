69bddb653c7209fc75ba5f48ad648a4992294a6b
owaissseeee <owaisesao@gmail.com>
2026-09-01 23:55:16 +0530

    Update Player.cs
    checking working

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public GameObject Player;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
