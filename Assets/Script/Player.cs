f423441bd15d8fa96b28a1b241b3fd4f0e0b9139
owaissseeee <owaisesao@gmail.com>
2026-09-02 20:55:01 +0530

    Added Player Movement And Simple Shooting

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -1,17 +1,69 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class Player : MonoBehaviour
 {
-    public GameObject player;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [Header("Movement Settings")]
+    public float moveSpeed = 5f;
+
+    [Header("Shooting Settings")]
+    public GameObject projectilePrefab;
+    public Transform firePoint;
+    public float projectileSpeed = 15f;
+
+    private Rigidbody2D rb;
+    private Camera cam;
+    private Vector2 movement;
+    private Vector2 mousePos;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        cam = Camera.main;
+
+        rb.gravityScale = 0f;
     }
 
-    // Update is called once per frame
     void Update()
     {
         
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+       
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+
+       
+        if (Input.GetMouseButtonDown(0))
+        {
+            Shoot();
+        }
+    }
+
+    void FixedUpdate()
+    {
+        
+        rb.MovePosition(rb.position + movement.normalized * moveSpeed * Time.fixedDeltaTime);
+
+      
+        Vector2 lookDir = mousePos - rb.position;
+
+        // Subtract 90 degrees assuming the default Unity triangle sprite points UP
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = angle;
+    }
+
+    void Shoot()
+    {
+        if (projectilePrefab != null && firePoint != null)
+        {
+            GameObject projectile = Instantiate(projectilePrefab, firePoint.position, firePoint.rotation);
+            Rigidbody2D projRb = projectile.GetComponent<Rigidbody2D>();
+
+            if (projRb != null)
+            {
+                projRb.linearVelocity = firePoint.up * projectileSpeed;
+            }
+        }
     }
 }
